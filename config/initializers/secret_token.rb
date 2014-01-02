d1065e46e86206816cf7000a66a9bd20a952a875
HyunjaePark <Hyunjae.Park@access-company.com>
2014-01-02 18:13:40 +0900

    NEW PROJECT Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorBlog::Application.config.secret_token = '160e883efa9c5ecab91f4cbdc6528964ad29abfe722f15e7a5f85799359a8981439d10a40242181e965b17c9d1c3ea4d5fad46313b7d13514540b6594732c8a7'
